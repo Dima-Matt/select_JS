7935d2e78a850ff57e32277475c7dcaf7a6ed8d1
Dima Mironov <mattmars9548@gmail.com>
2024-05-27 20:46:33 +0300

    good work

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const options = [
     {
         label: 'Витебск', 
-        value: 2
+        value: 2,
+        group: 'Первая'
     },
     {
         label: 'Могилёв', 
-        value: 6
+        value: 6,
+        group: 'Первая'
     },
     {
         label: 'Гомель', 
-        value: 3
+        value: 3,
+        group: 'Вторая'
     },
     {
         label: 'Брест', 
-        value: 1
+        value: 1,
+        group: 'Третья'
     },
     {
         label: 'Гродно', 
@@ -22,6 +26,11 @@ const options = [
     {
         label: 'Минск', 
         value: 7
+    },
+    {
+        label: 'Орша', 
+        value: 2,
+        group: 'Первая'
     },
 ]
 
@@ -33,36 +42,75 @@ class Select {
         this.$label = document.querySelector('.select__label');
         this.$dropDown = document.querySelector('.select__drop-down');
 
-        this.itemsForDropDown = options.map(({label, value}) => {
-            return `<li data-id=${value}>${label}</li>`
+        // this.itemsForDropDown = options.map(({label, value}) => {
+        //     return `<li data-id=${value}>${label}</li>`
+        // }).join('');
+
+        this.itemsForDropDown = this.initGroup(options).map(([key, items]) => {
+            if(key) {
+                const groupList = items.map(({label, value}) => {
+                    return `<li data-id=${value}>${label}</li>`
+                }).join('');
+
+                return `<ul style="padding: 8px"><span style="color: gray">${key}</span>${groupList}</ul>`
+
+            } else {
+                return items.map(({label, value}) => {
+                    return `<li data-id=${value}>${label}</li>`
+                }).join('');
+            }
         }).join('');
-        
+    
         this.$dropDown.insertAdjacentHTML('afterbegin', this.itemsForDropDown);
 
         this.$select.addEventListener('click', (e) => {
-            if(e.target.classList.contains('select__label')) {
-                console.log('this is label');
+            if(e.target.classList.contains('select__label')) { 
+                this.$select.classList.toggle('active');
+                
             } else {
-                if(e.target.tagName.toLowerCase() === 'li') {
-                   
+
+                if(e.target.tagName.toLowerCase() === 'li') {  
                     this.selectItem(+e.target.dataset.id);
+                    this.close();
+                    
+                   
                 }
             }
-        })
-
+        });
     }
 
     selectItem(id) {
         const selectedElement = this.options.find(item => item.value === id);
-        console.log(selectedElement);
-        this.$label.innerHTML = selectedElement.label;
-        
-        
-
+      
+        this.$label.innerHTML = selectedElement.label;   
     }
 
+    open() {
+        this.$select.classList.add('active');
     }
+
+    close() {
+        this.$select.classList.remove('active')
+    }
+
+    initGroup(items) {
+        const group = new Map();
+
+        items.forEach((item) => {
+            if (!group.has(item.group)) {
+                group.set(item.group, [item])
+            } else {
+                group.set(item.group, [ ...group.get(item.group), item])
+            }
+        });
+
+        return Array.from(group.entries())
+    }
+
+}
 
 
 const customSelect = new Select('.select', options, '350px');
+
+
 
